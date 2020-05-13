5d70dc32a2296be2548f5e4fc7da6aba8763c00f
balaji.rachamadugu <balaji.rachamadugu@QT-INBG-LT0558.QUALITEST.LOCAL>
2020-05-13 19:30:28 +0530

    Commit for Selenium and Pom.xmml changes

--- Demo/src/test/java/Qualiframe/Qualiframe/TestClass1.java
+++ Demo/src/test/java/Qualiframe/Qualiframe/TestClass1.java
@@ -1,23 +1,36 @@
 package Qualiframe.Qualiframe;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestClass1 {
 
-	public static WebDriver driver;
+	public WebDriver driver;
 
 	@BeforeMethod
-	public void launchDriver() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Balaji\\Downloads\\Drivers\\chromedriver81.exe");
-		driver = new ChromeDriver();
+	public void launchDriver() throws MalformedURLException {
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "C:\\Balaji\\Downloads\\Drivers\\chromedriver.exe"); driver = new
+		 * ChromeDriver(); driver.manage().timeouts().implicitlyWait(10,
+		 * TimeUnit.SECONDS); driver.manage().window().fullscreen();
+		 */
+		DesiredCapabilities desCap = DesiredCapabilities.chrome();
+		desCap.setPlatform(Platform.WINDOWS);
+		driver = new RemoteWebDriver(new URL("http://192.168.1.5:4444/wd/hub"), desCap);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().fullscreen();
+		// driver.navigate().to("https://automationtalks.com/");
 	}
 
 	@Test
@@ -25,14 +38,13 @@ public class TestClass1 {
 		driver.navigate().to("https://automationtalks.com/");
 		System.out.println(driver.getTitle());
 		System.out.println("Test 1 Title is " + driver.getTitle());
-
 	}
 
 	@Test
 	public void Test2() {
 		driver.navigate().to("https://automationtalks.com/");
 		System.out.println("Test 2 Title is " + driver.getTitle());
-		System.out.println("Test 2 Title is " + driver.getCurrentUrl());
+		System.out.println("Test 2 URL is " + driver.getCurrentUrl());
 	}
 
 	@Test
